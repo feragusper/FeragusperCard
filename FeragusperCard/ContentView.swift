54728ea91773cbc6795d9621418929a501bfc707
Fernando Perez <feragusper@gmail.com>
2021-05-21 21:28:32 -0300

    Initial Commit

---
+++ FeragusperCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FeragusperCard
+//
+//  Created by Fernando Perez on 21/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
