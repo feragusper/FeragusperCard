500563646fd784290b7967e99545a79082fcb06d
Fernando Perez <fernando.perez@intive.com>
2021-08-19 23:31:26 -0300

    latest changes to complete the challenge

--- FeragusperCard/ContentView.swift
+++ FeragusperCard/ContentView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.09, green: 0.63, blue: 0.52)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("fernando_perez_avatar")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                Text("Fernando Perez")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("Mobile Engineer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "+54 9 11 3 687 5018", imageName: "phone.fill")
+                InfoView(text: "feragusper@gmail.com", imageName: "envelope.fill")
+                
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
---
+++ FeragusperCard/InfoView.swift
@@ -0,0 +1,33 @@
+//
+//  InfoView.swift
+//  FeragusperCard
+//
+//  Created by Fernando Perez on 22/05/2021.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height:50)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.green)
+                Text(text)
+                    .foregroundColor(Color("Info Color"))
+            })
+            .padding(.all)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
